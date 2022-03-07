7a8598513cd53db975aaeb49b1a68df6fe7530b3
aleksandr <karpov.a1exandr@yandex.ru>
2022-03-07 22:13:42 +0300

    notes app - admin views

--- notes/app_notes/admin.py
+++ notes/app_notes/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_notes.models import Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "content",
+        "user",
+        "created_at",
+        "updated_at",
+        "is_published",
+    )
+    fields = (
+        "title",
+        "content",
+        "user",
+        "created_at",
+        "updated_at",
+        "is_published",
+    )
+    list_display_links = (
+        "id",
+        "title",
+    )
+    search_fields = (
+        "id",
+        "title",
+        "content",
+        "user",
+        "created_at",
+        "updated_at",
+        "is_published",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
--- notes/app_users/admin.py
+++ notes/app_users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_users.models import NoteUser
+
+
+@admin.register(NoteUser)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "date_joined",
+        "last_login",
+
+    )
+    fields = (
+        "email",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "date_joined",
+        "last_login",
+    )
+    list_display_links = (
+        "email",
+    )
+    search_fields = (
+        "email",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "date_joined",
+        "last_login",
+    )
+    readonly_fields = (
+        "date_joined",
+        "last_login",
+        "is_superuser",
+    )
